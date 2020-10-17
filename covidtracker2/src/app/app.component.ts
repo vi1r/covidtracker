0ffcaa7ed0e18df50509614d9e9517afacce6070
mattmcv <23mcvicker@gmail.com>
2020-10-16 19:26:53 -0700

    Add some data manipulation

--- covidtracker2/src/app/app.component.ts
+++ covidtracker2/src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {  
-  readonly ROOT_URL = 'https://api.covid19api.com/'
+  readonly ROOT_URL = 'https://api.covid19api.com/summary'
 
-  posts: any;
-
+  countries: any;
+  allCountries: Map<any, any>;
   constructor(private http: HttpClient) {
     this.http.get(this.ROOT_URL).toPromise().then(data => {
-      console.log(data);
+      this.countries = data["Countries"];
     });
   }
 }
